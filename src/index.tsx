272b41224db035ec3cc580d555127631be3db962
Armando Castillo <31118679+MandoCastillo@users.noreply.github.com>
2021-10-24 17:38:45 -0500

    code preview

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as esbuild from 'esbuild-wasm';
-import ReactDom from 'react-dom';
-import { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { useEffect, useRef, useState } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugins';
 import { fetchPlugin } from './plugins/fecth-pluging';
 
 const App = () => {
   const [input, setInput] = useState<string>('');
   const [code, setCode] = useState<string>('');
+  const iframe = useRef<any>();
 
   useEffect(() => {
     startService();
@@ -27,6 +28,7 @@ const App = () => {
   };
 
   const onClick = async () => {
+    iframe.current.srcdoc = html;
     const result = await esbuild.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -37,24 +39,66 @@ const App = () => {
         global: 'window'
       }
     });
-
-    // console.log(result);
-    setCode(result.outputFiles[0].text);
-
-    try {
-      eval(result.outputFiles[0].text);
-    } catch (e) {
-      alert(e)
-    }
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
 
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
+
   return <div>
-    <textarea style={{width:500, height:300}} value={input} onChange={event => setInput(event.target.value)} />
+    <textarea
+      style={{width: 500, height: 300}} value={input}
+      onChange={event => setInput(event.target.value)}
+    />
     <div>
       <button onClick={onClick}>Submit</button>
     </div>
-    <pre>{code}</pre>
+    <pre>{`
+// example to use
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  
+  const [number, setNumber] = useState(0);
+  
+  return (
+    <>
+      <h1>Counter {number}</h1>
+      <button onClick={()=> setNumber(number+1) }>+1</button>
+       <button onClick={()=> setNumber(number-1) }>-1</button>
+    </>
+  )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));`
+    }</pre>
+    <iframe
+      ref={iframe}
+      title="code preview"
+      sandbox="allow-scripts"
+      srcDoc={html}
+      frameBorder="1"
+    />
   </div>;
 };
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
